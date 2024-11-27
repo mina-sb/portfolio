fb080beea222c018c55aa1a313da402a5fd9d593
mina <110327510+mina-sb@users.noreply.github.com>
2024-11-27 22:31:00 +0330

    HomeInfo refactored

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,107 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
+import { stagesContent } from '../constants/homeInfo/index'
 
+const HomeInfo = ({ currentStage }) => {
+  const stage = stagesContent[currentStage]
+  if (!stage) return null
 
-const HomeInfo = ({currentStage}) => {
-  
-  if (currentStage === 1)
-    return (
-      <h1 className="text-3xl 	 md:text-6xl font-bold text-left py-20 md:py-6 px-8 -my-64 text-white mx-5 z-30">
-        Hi, I'm
-        <span className="font-bold mx-2 text-white">Mina</span>
-        <span className="block my-2 font-semibold md:font-bold text-2xl md:text-3xl md:mx-2 text-white">
-          {" "}
-          I'm Web developer
-        </span>
-        <Link
-          to="/about"
-          className=" inline-block text-white text-lg md:text-xl mx-0 md:mx-2 my-2 md:my-4 bg-transparent hover:bg-slate-500  font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded"
-        >
-          Learn more
-        </Link>
-      </h1>
-    );
+  return (
+    <h1 className="home-info-h1">
+      {stage.title}
+      {stage.subhead && <span className="home-info-gold-subhead">{stage.subhead}</span>}
+      <Link to={stage.link} className="home-info-button">
+        {stage.buttonText}
+      </Link>
+    </h1>
+  )
+}
 
-  if (currentStage === 2) {
-    return (
-      <h1 className="text-4xl  font-bold text-left py-20 md:py-6 px-8 -my-64 text-white mx-5 z-30">
-        Worked with many companies
-        <span className="block my-3 font-semibold md:font-bold text-xl md:text-3xl md:mx-2 text-white">
-          I'm Web developer
-        </span>
-        <Link
-          to="/about"
-          className=" inline-block text-white text-lg md:text-xl mx-0 md:mx-2 my-2 md:my-4 bg-transparent hover:bg-slate-500  font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded"
-        >
-          Learn more
-        </Link>
-      </h1>
-    );
-  }
-
-  if (currentStage === 3) {
-    return (
-      /*    <div className="mx-5  px-8 z-50 cursor-pointer bg-slate-400 top-10">
-        <h1 className="sm:text-4xl font-bold text-left py-4 mt-56 text-white">
-          Led multiple projects to <br /> success over the years. <br />
-          <span className="text-xl font-semibold">
-            Curious about the impact?
-          </span>
-        </h1>
-        <Link to="/about" className="text-white" onClick={handle}>
-          Visit my portfolio
-        </Link>
-      </div> */
-      <h1 className="sm:text-4xl font-bold text-left py-4 px-8 -my-64 text-white mx-5 z-30">
-        Led multiple projects to <br /> success over the years.
-        <span className="font-bold text-xl mx-2 text-white">
-          <br />
-          Curious about the impact?
-        </span>
-        <br />
-        <Link
-          to="/about"
-          className="text-xl inline-block my-4 bg-transparent hover:bg-slate-500 text-slate-300 font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded"
-        >
-          Visit my portfolio{" "}
-        </Link>
-      </h1>
-    );
-  }
-
-  if (currentStage === 4) {
-    return (
-      /*   <div className="mx-5  px-8 z-30 cursor-default -my-60">
-        <h1 className="sm:text-4xl  font-bold text-left py-4 mt-56 text-white">
-          Need a project done <br /> or looking for a dev? <br />
-          <span className="text-xl font-semibold">
-            I'm just a few keystrokes away{" "}
-          </span>
-        </h1>
-        <Link to="/about" className="text-white">
-          Let's talk
-        </Link>
-      </div> */
-      <h1 className="sm:text-4xl font-bold text-left py-4 px-8 -my-64 text-white mx-5 z-30">
-        Need a project done
-        <br />
-        or looking for a dev?
-        <br />
-        <span className="text-xl font-semibold">
-          I'm just a few keystrokes away
-        </span>
-        <br /> 
-        <Link
-          to="/about"
-          className="text-white text-xl inline-block  my-4 bg-transparent hover:bg-slate-500 text-slate-300 font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded"
-        >
-          {" "}
-          Let's talk
-        </Link>
-      </h1>
-    );
-  }
-
-  return null;
-};
-
-export default HomeInfo;
+export default HomeInfo
---
+++ src/constants/homeInfo/index.jsx
@@ -0,0 +1,38 @@
+export const stagesContent = {
+  1: {
+    title: (
+      <>
+        Hi, I'm
+        <span className="home-info-gold-span gold-gradient-text">Mina</span>
+      </>
+    ),
+    subhead: "I'm Web developer",
+    buttonText: 'About Me',
+    link: '/about'
+  },
+  2: {
+    title: (
+      <>
+        Not just a coder,
+        <span className="home-info-gold-subhead">
+          I build
+          <span className="gold-gradient-text home-info-gold-span">solutions!</span>
+        </span>
+      </>
+    ),
+    subhead: '',
+    buttonText: 'See My Work',
+    link: '/projects'
+  },
+  3: {
+    title: (
+      <>
+        Let’s
+        <span className="home-info-gold-span gold-gradient-text">connect,</span>
+      </>
+    ),
+    subhead: 'And innovate together!',
+    buttonText: 'Get in Touch',
+    link: '/contact'
+  }
+}

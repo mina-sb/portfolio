8f2315f9c19c2c8d81759223941e0ef1ece5265e
mina <110327510+mina-sb@users.noreply.github.com>
2024-11-28 00:20:29 +0330

    Navbar styles improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,40 @@
-import React, { useEffect, useRef, useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useEffect, useRef, useState } from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const sideNavRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false)
+  const sideNavRef = useRef(null)
 
   const toggleMenu = () => {
-    setIsOpen((prevState) => !prevState);
-  };
+    setIsOpen((prevState) => !prevState)
+  }
 
   useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener('mousedown', handleClickOutside)
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, []);
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
 
   function handleClickOutside(event) {
     if (
       sideNavRef.current &&
       !sideNavRef.current.contains(event.target) &&
-      !event.target?.closest("button")?.classList?.contains("menuBtn")
+      !event.target?.closest('button')?.classList?.contains('menuBtn')
     ) {
-      setIsOpen(false);
+      setIsOpen(false)
     }
   }
 
   return (
     <header className="header">
-      <NavLink
-        to="/"
-        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
-      >
-        <p className="font-bold text-xl blue-gradient_text">M</p>
+      <NavLink to="/" className="navbar-logo-link">
+        <p className="navbar-logo">M</p>
       </NavLink>
       <button
         data-collapse-toggle="navbar-solid-bg"
         type="button"
-        class="menuBtn inline-flex items-center p-2 w-10 h-10  justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="menuBtn navbar-btn"
         aria-controls="navbar-solid-bg"
         aria-expanded="false"
         onClick={toggleMenu}
@@ -59,103 +56,81 @@ const Navbar = () => {
           />
         </svg>
       </button>
-      {
-        <div
-          ref={sideNavRef}
-          className={`fixed bg-opacity-95 shadow-[rgba(0,0,5,0.1)_6px_5px_4px_0px]  md:hidden left-0 top-0 w-[60%] h-full pl-8 pt-8 bg-slate-600 ease-in-out transition-[margin-left] duration-300 
-          ${isOpen ? "ml-0 " : "ml-[-250px]"}`}
+      <div
+        ref={sideNavRef}
+        className={`navbar-side-menu ${isOpen ? 'translate-x-0' : '-translate-x-[100%]'}`}
+      >
+        <NavLink to="/" className="navbar-logo-link">
+          <p className="navbar-logo">M</p>
+        </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            `${isActive ? 'gold-gradient-text' : 'text-white'} block mb-3 mt-3 font-bold`
+          }
+          onClick={toggleMenu}
         >
-          <span
-            to="/"
-            className="w-10 h-10 -mt-4 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
-          >
-            <p className="font-bold text-xl blue-gradient_text">M</p>
-          </span>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              `${
-                isActive ? "blue-gradient_text font-bold" : "text-white block"
-              } block mb-3 mt-3`
-            }
-            onClick={toggleMenu}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/about"
-            className={({ isActive }) =>
-              `${
-                isActive ? "blue-gradient_text font-bold" : "text-white block"
-              } block mb-3`
-            }
-            onClick={toggleMenu}
-          >
-            About
-          </NavLink>
-          <NavLink
-            to="/projects"
-            className={({ isActive }) =>
-              `${
-                isActive ? "blue-gradient_text font-bold" : "text-white block"
-              } block  mb-3`
-            }
-            onClick={toggleMenu}
-          >
-            Projects
-          </NavLink>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) =>
-              `${
-                isActive ? "blue-gradient_text font-bold" : "text-white block"
-              } block mb-3`
-            }
-            onClick={toggleMenu}
-          >
-            Contact
-          </NavLink>
-        </div>
-      }
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            `${isActive ? 'gold-gradient-text' : 'text-white'} block mb-3 ont-bold`
+          }
+          onClick={toggleMenu}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            `${isActive ? 'gold-gradient-text' : 'text-white'} block  mb-3  font-bold`
+          }
+          onClick={toggleMenu}
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            `${isActive ? 'gold-gradient-text' : 'text-white'} block mb-3  font-bold`
+          }
+          onClick={toggleMenu}
+        >
+          Contact
+        </NavLink>
+      </div>
 
       <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
         <nav className="flex text-lg gap-7 font-medium">
           <NavLink
             to="/"
-            className={({ isActive }) =>
-              isActive ? "blue-gradient_text font-bold" : "text-white"
-            }
+            className={({ isActive }) => (isActive ? 'gold-gradient-text font-bold' : 'text-white')}
           >
             Home
           </NavLink>
           <NavLink
             to="/about"
-            className={({ isActive }) =>
-              isActive ? "blue-gradient_text font-bold" : "text-white"
-            }
+            className={({ isActive }) => (isActive ? 'gold-gradient-text font-bold' : 'text-white')}
           >
             About
           </NavLink>
           <NavLink
             to="/projects"
-            className={({ isActive }) =>
-              isActive ? "blue-gradient_text font-bold" : "text-white"
-            }
+            className={({ isActive }) => (isActive ? 'gold-gradient-text font-bold' : 'text-white')}
           >
             Projects
           </NavLink>
           <NavLink
             to="/contact"
-            className={({ isActive }) =>
-              isActive ? "blue-gradient_text font-bold" : "text-white"
-            }
+            className={({ isActive }) => (isActive ? 'gold-gradient-text font-bold' : 'text-white')}
           >
             Contact
           </NavLink>
         </nav>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
